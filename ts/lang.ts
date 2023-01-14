5295ecec7ce22187206213b291103564bd0bdef0
sealleci <ratratkin@gmail.com>
2023-01-14 22:05:20 +0800

    refact music app

---
+++ ts/lang.ts
@@ -0,0 +1,66 @@
+type Dictionary<K extends string | number | symbol = string, V = string> = { [key in K]?: V }
+type L10nRawData = {
+    language: string
+    fields: Dictionary
+}
+
+const FIELD_KEYS = [''] as const
+
+/**
+ * Dictionary of key-value pair `(language, field_value)`.
+ * 
+ * Usage:
+ * ``` js
+ * button_l10n_filed.speak('EN') // get "Click"
+ * ```
+ */
+class L10nField {
+    private data: Dictionary
+
+    constructor(data: Dictionary) {
+        this.data = data
+    }
+
+    public speak(key: string): string {
+        if (key in Object.keys(this.data)) {
+            const value = this.data[key]
+            return value !== undefined ? value : 'VALUE_MISSING'
+        }
+        return 'KEY_ERROR'
+    }
+}
+
+/**
+ * Dictionary of key-value pair `(field_key, l10n_field)`.
+ * 
+ * Usage:
+ * ``` js
+ * l10n_book.consult('BUTTON_TEXT').speak('EN') // get "Click"
+ * ```
+ */
+class L10nBook {
+    private data: Dictionary<typeof FIELD_KEYS[number], L10nField>
+
+    constructor(...raw_data: readonly L10nRawData[]) {
+        this.data = {}
+
+        for (const field_key of FIELD_KEYS) {
+            const page: Dictionary = {}
+
+            for (const raw_page of raw_data) {
+                if (raw_page.language !== '') {
+                    page[raw_page.language] = raw_page.fields[field_key]
+                }
+            }
+
+            this.data[field_key] = new L10nField(page)
+        }
+    }
+
+    public consult(key: typeof FIELD_KEYS[number]): L10nField {
+        let field = this.data[key]
+        return field !== undefined ? field : new L10nField({})
+    }
+}
+
+export { L10nBook }
--- ts/main.ts
+++ ts/main.ts
@@ -14,37 +14,64 @@ class BlockTree {
 
 }
 
+// TODO: 按钮绑定都放到App的构造函数里
+interface IWatchApp {
+    id: string
+    onEnter(): void
+    onExit(): void
+}
+
 class Watch {
-
+    app_list: IWatchApp[]
+    constructor() {
+        this.app_list = []
+    }
 }
 
-interface WatchApp {
+class BatteryApp implements IWatchApp {
+    id: string
+    constructor() {
+        this.id = ''
+    }
 
+    onEnter() {
+
+    }
+
+    onExit() {
+
+    }
 }
 
-class BatteryApp implements WatchApp {
-
-}
-
-class ClockApp implements WatchApp {
+class ClockApp implements IWatchApp {
+    id: string
     private static HANZI_NUMBERS: readonly string[]
 
     constructor() {
+        this.id = ''
         ClockApp.HANZI_NUMBERS =
             ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十']
     }
 
+    onEnter() {
+
+    }
+
+    onExit() {
+
+    }
+
     static convertNumToHanzi(num: number): string {
         if (num >= 0 && num <= 10) {
-            return ClockApp.HANZI_NUMBERS[num];
+            return ClockApp.HANZI_NUMBERS[num]
         } else if (num <= 19) {
             return '十' + ClockApp.HANZI_NUMBERS[num % 10]
         } else if (num === 20) {
-            return '二十';
+            return '二十'
         } else if (num <= 29) {
             return '廿' + ClockApp.HANZI_NUMBERS[num % 20]
         } else if (num === 30) {
-            return '三十';
+            return '三十'
         } else if (num <= 31) {
             return '卅' + ClockApp.HANZI_NUMBERS[num % 30]
         } else {
@@ -96,7 +123,8 @@ interface JumpGameBlock {
     is_pass: boolean
 }
 
-class JumpGameApp implements WatchApp {
+class JumpGameApp implements IWatchApp {
+    id: string
     private player: HTMLElement
     private stage: HTMLElement
     private lane: HTMLElement
@@ -113,6 +141,7 @@ class JumpGameApp implements WatchApp {
     private eclipse: number
 
     constructor() {
+        this.id = ''
         this.player = document.querySelector('#jump-player')!
         this.stage = document.querySelector('#jump-stage')!
         this.lane = document.querySelector('#jump-objs')!
@@ -127,6 +156,14 @@ class JumpGameApp implements WatchApp {
         this.max_speed = 11
         this.cur_speed = this.max_speed
         this.eclipse = 0
+    }
+
+    onEnter() {
+
+    }
+
+    onExit() {
+
     }
 
     updateScore(score: number) {
@@ -175,7 +212,6 @@ class JumpGameApp implements WatchApp {
     }
 
     moveBlock(obj: JumpGameBlock) {
-        // TODO: 建造一个Obj类
         obj.element.style.right = `${parseInt(obj.element.style.right) + 6}px`
 
         if (obj.element.offsetLeft < -obj.element.clientWidth) {
@@ -244,7 +280,7 @@ class JumpGameApp implements WatchApp {
         this.is_started = false
         document.querySelectorAll('#jump-objs>.jump-obj').forEach((obj) => {
             clearInterval(parseInt(obj.getAttribute('timer') ?? ''))
-        });
+        })
         removeChildren(document.querySelector('#jump-objs')!)
         clearInterval(this.game_timer)
         clearInterval(this.jump_timer)
@@ -309,6 +345,266 @@ class JumpGameApp implements WatchApp {
     }
 }
 
+class PetRatApp implements IWatchApp {
+    id: string
+    private max_tail_deg: number
+    private min_tail_deg: number
+    private tail_deg: number
+    private is_locked: boolean
+    private pet_cnt: number
+    private left_eye_parts: readonly HTMLElement[]
+    private right_eye_parts: readonly HTMLElement[]
+    private tail: HTMLElement
+    private heart_list: HTMLElement
+    private pet_cnt_display: HTMLElement
+
+    constructor() {
+        this.id = ''
+        this.max_tail_deg = 60
+        this.min_tail_deg = 30
+        this.tail_deg = this.min_tail_deg
+        this.is_locked = false
+        this.pet_cnt = 0
+        this.left_eye_parts = [
+            document.querySelector('#pet-rat>div:nth-child(6)')!,
+            document.querySelector('#pet-rat>div:nth-child(7)')!
+        ]
+        this, this.right_eye_parts = [
+            document.querySelector('#pet-rat>div:nth-child(8)')!,
+            document.querySelector('#pet-rat>div:nth-child(9)')!
+        ]
+        this.tail = document.querySelector('#pet-rat>div:nth-child(5)')!
+        this.heart_list = document.querySelector('#heart-list')!
+        this.pet_cnt_display = document.querySelector('#pet-rat-cnt')!
+    }
+
+    onEnter() {
+
+    }
+
+    onExit() {
+
+    }
+
+    closeEyes() {
+        for (const part of this.left_eye_parts) {
+            part.style.display = 'none'
+        }
+        for (const part of this.right_eye_parts) {
+            part.style.display = 'block'
+        }
+    }
+
+    openEyes() {
+        for (const part of this.left_eye_parts) {
+            part.style.display = 'block'
+        }
+        for (const part of this.right_eye_parts) {
+            part.style.display = 'none'
+        }
+    }
+
+    shakeTail() {
+        if (this.is_locked) {
+            return
+        }
+
+        this.is_locked = true
+        this.tail_deg ^= this.min_tail_deg ^ this.max_tail_deg
+        this.tail.style.transform = `rotate(${this.tail_deg}deg)`
+
+        const heart = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+
+        heart.setAttribute('height', '24px')
+        heart.setAttribute('width', '24px')
+        heart.setAttribute('viewBox', '0 0 16 16')
+        path.setAttribute('d',
+            'M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z')
+        heart.append(path)
+        this.heart_list.append(heart)
+        setTimeout(() => {
+            this.heart_list.removeChild(heart)
+        }, 300)
+
+        this.pet_cnt += 1
+        this.pet_cnt_display.textContent = this.pet_cnt.toString()
+        sleep(100).then(() => {
+            this.is_locked = false
+        })
+    }
+}
+
+// TODO: 把嵌在html的icon分离出来
+class WeatherApp implements IWatchApp {
+    id: string
+
+    constructor() {
+        this.id = ''
+    }
+
+    onEnter() {
+
+    }
+
+    onExit() {
+
+    }
+}
+
+interface MusicInfo {
+    title: string
+    author: string
+    len: number
+    color: string
+}
+
+class MusicApp implements IWatchApp {
+    id: string
+    private cur_index: number
+    private play_intv: number
+    private play_timer: number | undefined
+    private cur_duration: number
+    private is_playing: boolean
+    private play_btn: HTMLElement
+    private pause_btn: HTMLElement
+    private music_progress_bar: HTMLElement
+    private music_dot: HTMLElement
+    private music_slide: HTMLElement
+    private title_node: HTMLElement
+    private author_node: HTMLElement
+    private duration_node: HTMLElement
+    private cur_duration_node: HTMLElement
+    private progress_node: HTMLElement
+    private music_infos: MusicInfo[]
+
+    constructor() {
+        this.id = ''
+        this.cur_index = 0
+        this.play_intv = 250
+        this.play_timer = undefined
+        this.cur_duration = 0
+        this.is_playing = false
+        this.play_btn = document.querySelector('#music-go')!
+        this.pause_btn = document.querySelector('#music-stop')!
+        this.music_progress_bar = document.querySelector('#music-prog')!
+        this.music_dot = document.querySelector('#music-dot')!
+        this.music_slide = document.querySelector('#music-slide')!
+        this.title_node = document.querySelector('#music-title>span:nth-child(1)')!
+        this.author_node = document.querySelector('#music-title>span:nth-child(2)')!
+        this.duration_node = document.querySelector('#music-time-row>.music-time:nth-child(2)')!
+        this.cur_duration_node = document.querySelector('#music-time-row>.music-time:nth-child(1)')!
+        this.progress_node = document.querySelector('#music-prog')!
+        this.music_infos = []
+    }
+
+    onEnter() {
+
+    }
+
+    onExit() {
+
+    }
+
+    /**
+     * Convernt the given seconds to the time string.
+     * @returns The string presents the time with the "mm:ss" format.
+     * 
+     * Usage:
+     * ``` js
+     * converntSecondsToTime(70) // "01:10"
+     * ```
+     */
+    static converntSecondsToTime(seconds: number): string {
+        return `${padLeft(
+            Math.floor(seconds / 60).toString(), 2, '0')}:${padLeft(
+                Math.floor(seconds % 60).toString(), 2, '0')}`
+    }
+
+    updateCurDuration(duration: number) {
+        this.cur_duration = duration
+        this.cur_duration_node.textContent = MusicApp.converntSecondsToTime(this.cur_duration)
+    }
+
+    togglePlayBtn(is_playing: boolean) {
+        if (is_playing) {
+            this.play_btn.style.display = 'none'
+            this.pause_btn.style.display = 'flex'
+
+        } else {
+            this.play_btn.style.display = 'flex'
+            this.pause_btn.style.display = 'none'
+        }
+    }
+
+    initMusicProgress() {
+        this.music_progress_bar.style.width = '0px'
+        this.music_dot.style.left = '0px'
+    }
+
+    loadMusicInfos(index: number) {
+        this.pauseMusic()
+        this.updateCurDuration(0)
+        this.title_node.textContent = this.music_infos[index].title
+        this.author_node.textContent = this.music_infos[index].author
+        this.duration_node.textContent = MusicApp.converntSecondsToTime(this.music_infos[index].len)
+        this.progress_node.style.backgroundColor = this.music_infos[index].color
+        this.initMusicProgress()
+    }
+
+    playMusic() {
+        this.is_playing = true
+        this.togglePlayBtn(true)
+        this.play_timer = setInterval(this.playTick, this.play_intv)
+    }
+
+    pauseMusic() {
+        clearInterval(this.play_timer)
+        this.togglePlayBtn(false)
+        this.is_playing = false
+    }
+
+    playTick() {
+        // TODO: musics结构
+        this.updateCurDuration(Math.min(this.cur_duration + 1,
+            this.music_infos[this.cur_index].len))
+        const cur_width = Math.floor(this.cur_duration /
+            this.music_infos[this.cur_index].len * this.music_slide.clientWidth)
+        this.music_progress_bar.style.width = `${cur_width}px`
+        if (cur_width > 8) {
+            this.music_dot.style.left = `${cur_width - 8}px`
+        }
+        if (this.cur_duration >= this.music_infos[this.cur_index].len) {
+            this.pauseMusic()
+        }
+    }
+
+    clickPlayBtn() {
+        this.is_playing = !(this.is_playing)
+        if (this.is_playing) {
+            if (this.cur_duration === this.music_infos[this.cur_index].len) {
+                this.updateCurDuration(0)
+                this.initMusicProgress()
+            }
+            this.playMusic()
+        } else {
+            this.pauseMusic()
+        }
+    }
+
+    clickPrevoiusBtn() {
+        this.cur_index =
+            (this.cur_index - 1 + this.music_infos.length) % this.music_infos.length
+        this.loadMusicInfos(this.cur_index)
+    }
+
+    clickNextBtn() {
+        this.cur_index =
+            (this.cur_index + 1 + this.music_infos.length) % this.music_infos.length
+        this.loadMusicInfos(this.cur_index)
+    }
+}
+
 class Phone {
 
 }
@@ -333,5 +629,5 @@ const game = new Game()
 // Entrance.
 document.addEventListener('DOMContentLoaded', async () => {
     game.init();
-    (document.querySelector('#jump-btn')! as HTMLElement).addEventListener('mousedown', () => { })
+    (document.querySelector('#jump-btn') as HTMLElement).addEventListener('mousedown', () => { })
 })
